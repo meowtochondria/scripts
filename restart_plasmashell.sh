e821d6ee2d8a4de6407e262a9e8eb57c57664708
Dev Ghai <talk@devghai.com>
2022-11-29 22:37:27 -0800

    Create log file if it does not exist before restarting plasmashell.

--- restart_plasmashell.sh
+++ restart_plasmashell.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-LOG_FILE='/var/log/plasmashell'
+export PLASMA_LOG_FILE='/var/log/plasmashell'
 
 # Make an xrandr call as it kind of refreshes what's currently connected.
 xrandr --listactivemonitors &> /dev/null
 
-[ -f "$LOG_FILE" ] && truncate -s 0 $LOG_FILE
+if [ -f "$PLASMA_LOG_FILE" ]; then
+   truncate -s 0 $PLASMA_LOG_FILE
+else
+   /usr/lib/x86_64-linux-gnu/libexec/kf5/kdesu --noignorebutton -c "touch $PLASMA_LOG_FILE"
+   /usr/lib/x86_64-linux-gnu/libexec/kf5/kdesu --noignorebutton -c "chown $USER:$USER $PLASMA_LOG_FILE"
+fi
 
 if [ "$?" -ne "0" ]; then
-    kdialog --sorry "Unable to truncate $LOG_FILE. Please make sure the file exists and you have permissions to write to it."
+    kdialog --sorry "Unable to truncate $PLASMA_LOG_FILE. Please make sure the file exists and you have permissions to write to it."
     exit 1
 fi
 
 kill -s TERM $(pgrep plasmashell)
-plasmashell &> $LOG_FILE &
+plasmashell &> $PLASMA_LOG_FILE &
