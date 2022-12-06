3358a9a5aaee14fe3f544fa5e7f6238b350727fb
Dev Ghai <talk@devghai.com>
2022-12-06 12:58:51 -0800

    Add script to kill inactive sessions.

---
+++ kill_dangling_sessions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to kill dangling sessions with same login as the user.
+for session in $(/usr/bin/loginctl list-sessions --output=json --no-pager | jq ".[] | select(.user == \"$USER\") | .session" | tr -d '"'); do
+    state=$(/usr/bin/loginctl session-status $session | grep -oP 'State: .+' | cut -f 2 -d ' ')
+    echo "$state"; [ "$state" != "active" ] && /usr/bin/loginctl kill-session $session
+done
+
+/usr/bin/notify-send "Sessions" "$(/usr/bin/loginctl list-sessions)
