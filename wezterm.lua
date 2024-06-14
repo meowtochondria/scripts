5dd8f1dff1aa51e2914274c3624ceb59fbf05e89
Dev Ghai <talk@devghai.com>
2024-06-14 04:41:07 -0700

    Add shortcut to rename tab

--- wezterm.lua
+++ wezterm.lua
@@ -37,9 +37,34 @@ config.tab_max_width = 50
 
 -- Some custom keyboard shortcuts
 config.keys = {
-  { key = '\\', mods = 'CTRL', action = act.SplitHorizontal{domain='CurrentPaneDomain'} },
---   { key = '\\', mods = 'CTRL|SHIFT', action = act.SplitVertical{domain='CurrentPaneDomain'} },
+  { 
+      key = '\\',
+      mods = 'CTRL',
+      action = act.SplitHorizontal{domain='CurrentPaneDomain'}
+  },
+--   { 
+--      key = '\\',
+--      mods = 'CTRL|SHIFT',
+--      action = act.SplitVertical{domain='CurrentPaneDomain'}
+--   },
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 }
+
+
 
 -- Disable audible beep
 config.audible_bell = "Disabled"
