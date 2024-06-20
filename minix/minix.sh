9e5f5c080c6d5fe126c6f39271d00291ebc3ba83
Dev Ghai <talk@devghai.com>
2024-06-20 03:04:22 -0700

    Some debs and script to configure server.

---
+++ minix/minix.sh
@@ -0,0 +1,33 @@
+# ubuntu-minimal-22.04
+
+sudo apt install man man-db manpages bash-completion vim python3 python3-pip mlocate dnsutils netcat apt-utils dialog net-tools apt-file
+
+# fix man in ubuntu-minimal
+test -x /usr/bin/man.REAL && sudo mv /usr/bin/man{.REAL,}
+sudo systemctl start man-db.service
+
+# remove useless motd
+sudo rm /etc/update-motd.d/60-unminimize /etc/update-motd.d/10-help-text
+echo "ENABLED=0" | sudo tee -a /etc/default/motd-news
+
+# Update ntp pool. Make sure firewall allows egress on port 123
+sudo mkdir /etc/systemd/timesyncd.conf.d
+
+echo -e "[Time]\nNTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org\nFallbackNTP=ntp.ubuntu.com" | sudo tee -a /etc/systemd/timesyncd.conf.d/pools.conf
+
+sudo systemctl restart systemd-timesyncd.service
+timedatectl set-ntp true
+
+# mounting USB devices. unmount using pumount
+sudo apt install pmount
+usb_device='/dev/sda' # find using lsblk
+for d in $(lsblk --output path,label --list --noheadings $usb_device | tail -n +2 | tr -s ' ' ','); do path=${d%,*}; label=${d#*,}; pmount $path $label; echo "path=$path; label=$label; mountpoint=/media/$label"; done
+# other packages to install that do not have repos
+# caddy, cloudflared
+
+# cloudflared
+# 1. install
+# 2. setup config
+# 3. cloudflared service install
+
+# caddy is not needed if using cloudflare tunnels. no https management is needed either.
