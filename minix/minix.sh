5acc9b2a1f663b5e642cb68c82068a3c5f10639a
Dev Ghai <talk@devghai.com>
2024-07-01 03:34:05 -0700

    working setup

--- minix/minix.sh
+++ minix/minix.sh
@@ -38,24 +38,83 @@ sudo mkdir -p /opt/pipx/bin
 sudo chmod -R a+rwx /opt/pipx
 echo -e 'export PIPX_HOME="/opt/pipx"\nexport PIPX_BIN_DIR="$PIPX_HOME/bin"\n[[ "$PATH" == *"$PIPX_BIN_DIR"* ]] || export PATH="$PIPX_BIN_DIR:$PATH"' | sudo tee -a /etc/profile.d/pipx.sh
 
+# Group for radicale and vdirsyncer. This is used in systemd definitions.
+sudo addgroup cal
+sudo adduser --system --no-create-home --disabled-login --ingroup=cal cal
+
 ##### VDIRSYNCER using PIPX #####
 pipx install vdirsyncer
 pipx inject vdirsyncer aiohttp-oauthlib
-sudo adduser --system --no-create-home --disabled-login --group vdirsyncer
-sudo install --directory --group=vdirsyncer --owner=vdirsyncer /etc/vdirsyncer
-sudo install --directory --group=vdirsyncer --owner=vdirsyncer /var/log/vdirsyncer
-sudo install --directory --group=vdirsyncer --owner=vdirsyncer /var/lib/vdirsyncer
+sudo install --directory --owner=cal --group=cal /etc/vdirsyncer
+sudo install --directory --owner=cal --group=cal /var/log/vdirsyncer
+sudo install --directory --owner=cal --group=cal /var/lib/calendar/vdirsyncer
 
 # SCP config file to /etc/vdirsyncer/config
-scp ./vdirsyncer/config minix:~/vdirsyncer.config
-scp ./vdirsyncer/gcal_token minix:~/gcal_token
+# scp ./vdirsyncer/config minix:~/vdirsyncer.config
+# scp ./vdirsyncer/gcal_token minix:~/gcal_token
 sudo mv ~/vdirsyncer.config /etc/vdirsyncer/config
-sudo mv ~/gcal_token /etc/vdirsyncer/gcal_token
-sudo chown vdirsyncer:vdirsyncer /etc/vdirsyncer/*
+sudo mv ~/gcal_token /var/lib/vdirsyncer/gcal_token
+sudo chown cal:cal /etc/vdirsyncer/*
 
 # SCP systemd files
 scp ./vdirsyncer/etc.systemd.system.vdirsyncer.service minix:~/vdirsyncer.service
 scp ./vdirsyncer/etc.systemd.system.vdirsyncer.timer minix:~/vdirsyncer.timer
 sudo mv ~/vdirsyncer.service /etc/systemd/system/
 sudo mv ~/vdirsyncer.timer /etc/systemd/system/
+
+# Reload systemctl and start service
+sudo systemctl daemon-reload
+sudo systemctl enable vdirsyncer.{timer,service}
+sudo systemctl start vdirsyncer.{timer,service}
+
+##### RADICALE using PIPX #####
+pipx install radicale
+pipx inject radicale https://github.com/Unrud/RadicaleInfCloud/archive/master.tar.gz
+
+sudo adduser --system --no-create-home --disabled-login --ingroup cal cal
+sudo install --directory --owner=cal --group=cal /etc/radicale
+sudo install --directory --owner=cal --group=cal /var/log/radicale
+sudo install --directory --owner=cal --group=cal /var/lib/calendar/radicale
+
+# SCP config file to /etc/radicale/config
+# scp ./radicale/config minix:~/radicale.config
+sudo mv ~/radicale.config /etc/radicale/config
+sudo chown cal:cal /etc/radicale/*
+
+# SCP systemd files
+# scp ./radicale/etc.systemd.system.radicale.service minix:~/radicale.service
+sudo mv ~/radicale.service /etc/systemd/system/
+
+# Reload systemctl and start service
+sudo systemctl daemon-reload
+sudo systemctl enable radicale.service
+sudo systemctl start radicale.service
+
+# SETUP BETWEEN VDIRSYNCER AND RADICALE:
+# vdirsyncer syncs normally to its own directory.
+# radicale has a link pointing to calendar i want to expose into vdirsyncer directory.
+# However, radicale needs one file and one directory in calendar directory which we will
+# create using commands.
+# This setup is as optimal as it can get to do as few network requests as possible on an
+# Atom powered fanless machine.
+# Caveat is that if a radicale sync is happening to modify an event, and vdirsyncer sync
+# is happening at the same time, then the behavior is undefined. I am not planning to
+# modify events in exposed calendar using radicale.
+
+# MANUAL STEPS TO SETUP CALENDAR AND LINKING VDIRSYNCER TO RADICALE
+sudo -Hu cal /opt/pipx/bin/vdirsyncer --config /etc/vdirsyncer/config discover
+# say yes and then do first sync
+sudo systemctl start vdirsyncer && sudo journalctl --follow -xeu vdirsyncer
+
+# at this point, /var/lib/vdirsyncer/calendars should contain the calendar i want to expose using radicale.
+# now create the file and direcory that radicale needs.
+echo '{"C:calendar-description": "work calendar", "C:supported-calendar-component-set": "VEVENT,VJOURNAL,VTODO", "D:displayname": "okta", "ICAL:calendar-color": "#2958ceff", "tag": "VCALENDAR"}' | sudo -u cal tee -a /var/lib/calendar/vdirsyncer/calendars/okta/.Radicale.props
+sudo chmod 0600 /var/lib/calendar/vdirsyncer/calendars/okta/.Radicale.props
+sudo install --directory --owner=cal --group=cal /var/lib/calendar/vdirsyncer/calendars/okta/.Radicale.cache
+
+
+# now link it. this expects a user called dev to exist, and so does the directory
+sudo install --directory --group=cal --owner=cal /var/lib/calendar/radicale/collections/collection-root/dev/
+sudo -Hu cal ln -sT /var/lib/calendar/vdirsyncer/calendars/okta /var/lib/calendar/radicale/collections/collection-root/dev/okta
+
 
