5b12e60dde725fe907b9a5216aab1b9d869e43c1
Dev Ghai <talk@devghai.com>
2024-09-12 14:17:58 -0700

    Pull CPanel API user from KeepassXC

--- update-namecheap-cert.sh
+++ update-namecheap-cert.sh
@@ -16,6 +16,8 @@ echo
 test -z "$KEEPASS" && echo "Empty password. Exiting." && exit 1
 
 CPANEL_API_TOKEN=$($KEEPASSXC_CLI show --quiet --attributes password $KEEPASSXC_DB '/Home/Namecheap CPanel API' <<< $KEEPASS)
+CPANEL_API_USER=$($KEEPASSXC_CLI show --quiet --attributes username $KEEPASSXC_DB '/Home/Namecheap CPanel API' <<< $KEEPASS)
+
 # See if we have the entry we want before proceeding. Acts as a check for password validity as well.
 test -z "$CPANEL_API_TOKEN" && echo "Couldn't fetch CPanel token. Wrong password or entry moved?" && exit 2
 
@@ -44,7 +46,7 @@ $LEGO_CLI --email=${EMAIL} --domains=devghai.com --domains=cal.devghai.com --dom
 
 # update all domains
 for domain in devghai.com ghai.co cloud.devghai.com git.devghai.com src.devghai.com; do
-    curl -H "Content-Type: text/plain" -H "Authorization: cpanel devgosxk:$CPANEL_API_TOKEN" "https://premium59.web-hosting.com:2083/execute/SSL/install_ssl" --data "domain=$domain" --data-urlencode "cert@certificates/devghai.com.crt" --data-urlencode "key@certificates/devghai.com.key" | jq .
+    curl -H "Content-Type: text/plain" -H "Authorization: cpanel ${CPANEL_API_USER}:${CPANEL_API_TOKEN}" "https://premium59.web-hosting.com:2083/execute/SSL/install_ssl" --data "domain=$domain" --data-urlencode "cert@certificates/devghai.com.crt" --data-urlencode "key@certificates/devghai.com.key" | jq .
 done
 
 unset NAMECHEAP_API_KEY
@@ -52,4 +54,6 @@ unset NAMECHEAP_API_USER
 unset NAMECHEAP_DEBUG
 unset CLOUDFLARE_DNS_API_TOKEN
 unset CPANEL_API_TOKEN
+unset CPANEL_API_USER
+
 rm -rf "$PWD/accounts"
